166f8fdcbd50bfc230b4a85440f6a3de15eb5796
vyshakkolloth <vyshakkolloth@gmail.com>
2026-07-28 07:39:55 +0530

    chumma

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -4,7 +4,7 @@ import { SectionCards } from "@/components/section-cards"
 import { OrdersManager } from "@/components/admin/orders-manager"
 
 export const metadata = {
-  title: "dashboard",
+  title: "dashboard|ikkayes",
   icons: {
     icon: "/logos/ikkayeslogo.png",
   },
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default async function Page() {
   return (
-    <div className="flex flex-1 flex-col">
+    <div className="flex flex-1 flex-col ">
       <div className="@container/main flex flex-1 flex-col gap-2 ">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 ">
           <SectionCards />
